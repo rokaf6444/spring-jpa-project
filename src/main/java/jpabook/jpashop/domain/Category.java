f577ba47c3ef4e47a5f4a5ccac44ebce0e845306
rokaf6444 <dino1106@naver.com>
2024-10-08 01:56:39 +0900

    feat: 상품 도메인 개발 완료

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -35,7 +35,7 @@ public class Category {
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
 
-    //== 연관 관계 편의 메서드==//
+    //==연관 관계 편의 메서드==//
     public void addChildCategory(Category child) {
         this.child.add(child);
         child.setParent(this);
